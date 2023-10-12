03af563d2c82c0eb167af08facfc9a7893c565b8
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2023-10-12 10:47:25 +0200

    Setup lein project

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject picoautomator "0.0.10"
+  :description "Functional API for pico-automator UI automation library"
+  :url "https://github.com/aivanovski/pico-automator-clojure"
+  :license {:name "Apache License Version 2.0"
+            :url "http://www.apache.org/licenses/"}
+
+  :repositories [["jitpack" "https://jitpack.io"]]
+
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.jetbrains.kotlin/kotlin-stdlib-jdk8 "1.8.22"]
+                 [com.github.aivanovski/pico-automator "0.0.9"]]
+
+  :repl-options {:init-ns picoautomator.core})
